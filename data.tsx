2527a58d8691a095d2413c89cdc7f936841ec06e
Héctor Tovar <h.tovarm07@gmail.com>
2025-03-08 19:45:14 -0600

    added the headbar

--- data.tsx
+++ data.tsx
@@ -1,46 +1,14 @@
-import { BookText, CodeSquare, HomeIcon, UserRound, Linkedin, Twitter, Rss, Twitch, Youtube, Crop, Pencil, Computer, Book, Rocket, Speech, Github, Mail } from "lucide-react";
+import {Instagram, Facebook} from "lucide-react";
 
 export const socialNetworks = [
     {
         id: 1,
-        logo: <Linkedin size={30} strokeWidth={1} />,
-        src: "https://www.linkedin.com/in/h%C3%A9ctor-eduardo-tovar-mendoza-a6b33a2b5/",
+        logo: <Instagram size={30} strokeWidth={1} />,
+        src: "https://www.instagram.com/unagi.sushi.teppanyaki/?hl=es",
     },
     {
         id: 2,
-        logo: <Github size={30} strokeWidth={1} />,
-        src: "https://github.com/htovarm7",
-    },
-    {
-        id: 3,
-        logo: <Mail size={30} strokeWidth={1}/>,
-        src: "mailto:h.tovarm07@gmail.com.com",
-    },
-    {
-        id: 4,
-        logo: <Twitch size={30} strokeWidth={1}/>,
-        src: "https://www.twitch.tv/lalomora3",
+        logo: <Facebook size={30} strokeWidth={1} />,
+        src: "https://www.facebook.com/profile.php?id=100027869035898",
     }
-];
-
-
-export const itemsNavbar = [
-    {
-        id: 1,
-        title: "Home",
-        icon: <HomeIcon size={25} color="#fff" strokeWidth={1} />,
-        link: "/",
-    },
-    {
-        id: 2,
-        title: "User",
-        icon: <UserRound size={25} color="#fff" strokeWidth={1} />,
-        link: "/about-me",
-    },
-    {
-        id: 3,
-        title: "Target",
-        icon: <CodeSquare size={25} color="#fff" strokeWidth={1} />,
-        link: "/portfolio",
-    },
 ];
